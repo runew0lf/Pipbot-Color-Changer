7703cb4bda3e3e07310cbf29efc9f3db36a2dee4
Runew0lf <runew0lf@gmail.com>
2018-12-12 15:45:18 +0000

    Added quickboy settings

--- pipboy.py
+++ pipboy.py
@@ -3,7 +3,6 @@ import os.path
 import shutil
 import sys
 
-
 from PyQt5.QtCore import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
@@ -14,11 +13,16 @@ SHGFP_TYPE_CURRENT = 0   # Get current, not default value
 buf = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
 ctypes.windll.shell32.SHGetFolderPathW(
     None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
-fpath = f"{buf.value}\\My Games\\Fallout 76\\Fallout76Prefs.ini"
-backuppath = f"{buf.value}\\My Games\\Fallout 76\\Fallout76Prefs.bkp"
 
-if os.path.isfile(backuppath) is False:
-    shutil.copyfile(fpath, backuppath)  
+prefs_ini = f"{buf.value}\\My Games\\Fallout 76\\Fallout76Prefs.ini"
+backup_ini = f"{buf.value}\\My Games\\Fallout 76\\Fallout76Prefs.bkp"
+custom_ini = f"{buf.value}\\My Games\\Fallout 76\\Fallout76Custom.ini"
+
+if os.path.isfile(backup_ini) is False:
+    shutil.copyfile(prefs_ini, backup_ini)
+
+if os.path.isfile(custom_ini) is False:
+    open(custom_ini, 'a').close()
 
 def percentage(part):
     return round(float(part)/float(255), 4)
@@ -29,38 +33,44 @@ class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         self.setGeometry(50, 50, 800, 400)
-        self.setWindowTitle("Fo76 PipBoy Color Editor")
+        self.setWindowTitle("Fo76 PipBoy Color Changer")
         self.setWindowIcon(QIcon('logo.png'))
         self.home()
 
     def home(self):
-        self.button = QPushButton("Set Color", self)
-        self.button.move(50, 150)
-        self.color_dialog = QColorDialog(self)
-        self.color_dialog.setWindowFlags(
-            self.color_dialog.windowFlags() & ~Qt.Dialog)
-        self.color_dialog.setOptions(
+        self.btnPipBoy = QPushButton("Set PipBoy Color", self)
+        self.btnPipBoy.move(50, 150)
+
+        self.btnQuickBoy = QPushButton("Set QuickBoy Color", self)
+        self.btnQuickBoy.move(50, 190)
+
+        self.dlgColor = QColorDialog(self)
+        self.dlgColor.setWindowFlags(
+            self.dlgColor.windowFlags() & ~Qt.Dialog)
+        self.dlgColor.setOptions(
             QColorDialog.DontUseNativeDialog | QColorDialog.NoButtons)
-        self.color_dialog.move(250, 0)
+        self.dlgColor.move(250, 0)
 
         self.text = QTextEdit(self)
         self.text.setFixedSize(200, 100)
         self.text.move(10, 10)
         self.text.setReadOnly(True)
 
-        self.button.clicked.connect(self.color_picker)
+        self.btnPipBoy.clicked.connect(self.set_pipboy_color)
+        self.btnQuickBoy.clicked.connect(self.set_quickboy_color)
+        
         self.show()
 
-    def color_picker(self):
-        color = self.color_dialog.currentColor()
+    def set_pipboy_color(self):
+        color = self.dlgColor.currentColor()
         r, g, b, a = color.getRgb()
         self.text.setPlainText(
             f"[Pipboy]\nfPipboyEffectColorR={percentage(r)}\nfPipboyEffectColorG={percentage(g)}\nfPipboyEffectColorB={percentage(b)}")
 
-        with open(fpath, "r") as fh:
+        with open(prefs_ini, "r") as fh:
             inifile = fh.read().splitlines()
 
-        writefh = open(fpath, "w")
+        writefh = open(prefs_ini, "w")
         for lines in inifile:
             if "fPipboyEffectColorR" in lines:
                 lines = f"fPipboyEffectColorR={percentage(r)}"
@@ -70,8 +80,36 @@ class Window(QMainWindow):
                 lines = f"fPipboyEffectColorB={percentage(b)}"
             writefh.write(lines + '\n')
         writefh.close()
+    
+    def set_quickboy_color(self):
+        color = self.dlgColor.currentColor()
+        r, g, b, a = color.getRgb()
+        self.text.setPlainText(
+            f"[Pipboy]\nfQuickBoyEffectColorR={percentage(r)}\nfQuickBoyEffectColorG={percentage(g)}\nfQuickBoyEffectColorB={percentage(b)}")     
+       
+        with open(custom_ini, "r") as fh:
+            inifile = fh.read().splitlines()
 
-
+        if any("fQuickBoyEffect" in s for s in inifile):
+            writefh = open(custom_ini, "w")
+            for lines in inifile:
+                if "nfQuickBoyEffectColorR" in lines:
+                    lines = f"nfQuickBoyEffectColorR={percentage(r)}"
+                if "nfQuickBoyEffectColorG" in lines:
+                    lines = f"nfQuickBoyEffectColorG={percentage(g)}"
+                if "nfQuickBoyEffectColorB" in lines:
+                    lines = f"nfQuickBoyEffectColorB={percentage(b)}"
+                writefh.write(lines + '\n')
+            writefh.close()
+        else:
+            writefh = open(custom_ini, "w")
+            writefh.write("[Pipboy]\n")
+            writefh.write(f"nfQuickBoyEffectColorR={percentage(r)}\n")          
+            writefh.write(f"nfQuickBoyEffectColorG={percentage(g)}\n")
+            writefh.write(f"nfQuickBoyEffectColorB={percentage(b)}\n")
+            for lines in inifile:
+                writefh.write(lines + '\n')
+            writefh.close()
 def run():
     app = QApplication(sys.argv)
     GUI = Window()
